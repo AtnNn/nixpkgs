60f4a807bda526bbc026da401423f788ce012ac0
Etienne Laurin <etienne@atnnn.com>
2019-04-27 09:34:45 +0000

    SWI-Prolog 8.0.2

--- pkgs/development/compilers/swi-prolog/default.nix
+++ pkgs/development/compilers/swi-prolog/default.nix
@@ -1,33 +1,31 @@
 { stdenv, fetchurl, jdk, gmp, readline, openssl, libjpeg, unixODBC, zlib
 , libXinerama, libarchive, db, pcre, libedit, libossp_uuid, libXft, libXpm
-, libSM, libXt, freetype, pkgconfig, fontconfig, makeWrapper ? stdenv.isDarwin
+, libSM, libXt, freetype, pkgconfig, fontconfig, cmake, libXext, makeWrapper ? stdenv.isDarwin
 }:
 
 let
-  version = "7.6.4";
+  version = "8.0.2";
 in
 stdenv.mkDerivation {
   name = "swi-prolog-${version}";
 
   src = fetchurl {
     url = "http://www.swi-prolog.org/download/stable/src/swipl-${version}.tar.gz";
-    sha256 = "14bq4sqs61maqpnmgy6687jjj0shwc27cpfsqbf056nrssmplg9d";
+    sha256 = "abb81b55ac5f2c90997c0005b1f15b74ed046638b64e784840a139fe21d0a735";
   };
 
   buildInputs = [ jdk gmp readline openssl libjpeg unixODBC libXinerama
-    libarchive db pcre libedit libossp_uuid libXft libXpm libSM libXt
-    zlib freetype pkgconfig fontconfig ]
+    libarchive db pcre libedit libossp_uuid libXft libXpm libSM libXt libXext
+    zlib freetype pkgconfig fontconfig cmake ]
   ++ stdenv.lib.optional stdenv.isDarwin makeWrapper;
 
-  hardeningDisable = [ "format" ];
+  # hardeningDisable = [ "format" ];
 
-  configureFlags = [
-    "--with-world"
-    "--enable-gmp"
-    "--enable-shared"
-  ];
+  cmakeFlags = "-DCMAKE_BUILD_TYPE=Release";
 
-  buildFlags = "world";
+  checkPhase = ''
+    ctest -j $NIX_BUILD_CORES --output-on-failure
+  '';
 
   # For macOS: still not fixed in upstream: "abort trap 6" when called
   # through symlink, so wrap binary.
@@ -44,9 +42,9 @@ stdenv.mkDerivation {
   meta = {
     homepage = http://www.swi-prolog.org/;
     description = "A Prolog compiler and interpreter";
-    license = "LGPL";
+    license = stdenv.lib.licenses.bsd2;
 
     platforms = stdenv.lib.platforms.unix;
-    maintainers = [ stdenv.lib.maintainers.meditans ];
+    maintainers = [ stdenv.lib.maintainers.meditans stdenv.lib.maintainers.atnnn ];
   };
 }
